719b895ced10513027df42f5138755772205ae07
Maxence Lange <maxence@artificial-owl.com>
2023-11-02 15:33:11 -0100

    fix psalm
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/public/DB/QueryBuilder/IQueryBuilder.php
+++ lib/public/DB/QueryBuilder/IQueryBuilder.php
@@ -744,7 +744,7 @@ interface IQueryBuilder {
 	 * </code>
 	 *
 	 * @param string $column The column into which the value should be inserted.
-	 * @param IParameter|string $value The value that should be inserted into the column.
+	 * @param ILiteral|IParameter|IQueryFunction $value The value that should be inserted into the column.
 	 *
 	 * @return $this This QueryBuilder instance.
 	 * @since 8.2.0
