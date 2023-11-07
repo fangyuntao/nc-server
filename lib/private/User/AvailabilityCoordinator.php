cafb3cb8b19cb8ee2247f87b362ee86d8a6c5cb6
Richard Steinmetz <richard@steinmetz.cloud>
2023-11-07 14:08:52 +0100

    feat(ocp): implement public API to get out of office data
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- lib/private/User/AvailabilityCoordinator.php
+++ lib/private/User/AvailabilityCoordinator.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
  * @copyright 2023 Christoph Wurst <christoph@winzerhof-wurst.at>
  *
  * @author 2023 Christoph Wurst <christoph@winzerhof-wurst.at>
+ * @author Richard Steinmetz <richard@steinmetz.cloud>
  *
  * @license GNU AGPL version 3 or any later version
  *
@@ -25,14 +26,87 @@ declare(strict_types=1);
 
 namespace OC\User;
 
+use DateTimeImmutable;
+use JsonException;
+use OCA\DAV\Db\AbsenceMapper;
+use OCP\AppFramework\Db\DoesNotExistException;
+use OCP\ICache;
+use OCP\ICacheFactory;
 use OCP\IUser;
 use OCP\User\IAvailabilityCoordinator;
 use OCP\User\IOutOfOfficeData;
 
 class AvailabilityCoordinator implements IAvailabilityCoordinator {
 
-	public function getCurrentOutOfOfficeData(IUser $user): ?IOutOfOfficeData {
-		return null;
+	private ICache $cache;
+
+	public function __construct(
+		ICacheFactory $cacheFactory,
+		private AbsenceMapper $absenceMapper,
+	) {
+		$this->cache = $cacheFactory->createLocal('OutOfOfficeData');
 	}
 
+	/**
+	 * @throws JsonException
+	 */
+	private function getCachedOutOfOfficeData(IUser $user): ?OutOfOfficeData {
+		$cachedString = $this->cache->get($user->getUID());
+		if ($cachedString === null) {
+			return null;
+		}
+
+		$cachedData = json_decode($cachedString, true, 10, JSON_THROW_ON_ERROR);
+		return new OutOfOfficeData(
+			$cachedData['id'],
+			$user,
+			$cachedData['startDate'],
+			$cachedData['endDate'],
+			$cachedData['shortMessage'],
+			$cachedData['message'],
+		);
+	}
+
+	/**
+	 * @throws JsonException
+	 */
+	private function setCachedOutOfOfficeData(IOutOfOfficeData $data): void {
+		$this->cache->set(
+			$data->getUser()->getUID(),
+			json_encode([
+				'id' => $data->getId(),
+				'startDate' => $data->getStartDate(),
+				'endDate' => $data->getEndDate(),
+				'shortMessage' => $data->getShortMessage(),
+				'message' => $data->getMessage(),
+			], JSON_THROW_ON_ERROR),
+			300,
+		);
+	}
+
+	public function getCurrentOutOfOfficeData(IUser $user): ?IOutOfOfficeData {
+		$cachedData = $this->getCachedOutOfOfficeData($user);
+		if ($cachedData !== null) {
+			return $cachedData;
+		}
+
+		try {
+			$absenceData = $this->absenceMapper->findByUserId($user->getUID());
+		} catch (DoesNotExistException $e) {
+			return null;
+		}
+
+		$startDate = new DateTimeImmutable($absenceData->getFirstDay());
+		$endDate = new DateTimeImmutable($absenceData->getLastDay());
+		$data = new OutOfOfficeData(
+			(string)$absenceData->getId(),
+			$user,
+			$startDate->getTimestamp(),
+			$endDate->getTimestamp(),
+			$absenceData->getStatus(),
+			$absenceData->getMessage(),
+		);
+		$this->setCachedOutOfOfficeData($data);
+		return $data;
+	}
 }
